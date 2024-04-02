b0040889748fda80527e4d86de1cc84d6c5bf7ce
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2024-04-02 12:23:20 +0500

    header done

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,32 @@
+import Image from "next/image";
 import React from "react";
 import "./style.css";
+import img1 from "../imgs/header-img/Employees.png";
+import img2 from "../imgs/header-img/Project.png";
+import img3 from "../imgs/header-img/Team Strength.png";
+import img4 from "../imgs/header-img/Group 1000000805.png";
+import img5 from "../imgs/header-img/Graph.png";
 
 export const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <div className="top">
+        <h2>Good Morning Anima</h2>
+        <p>Hope you have a good day</p>
+      </div>
+      <div className="middle">
+        {/* <Image src={img3} /> */}
+        <div className="imgBox">
+          <p>Teams Strength</p>
+          <Image src={img5} className="img5" />
+          <Image src={img4} className="img4" />
+        </div>
+
+        <Image src={img1} className="img1" />
+      </div>
+      <div className="footer">
+        <Image src={img2} />
+      </div>
+    </div>
+  );
 };
--- components/RightSide.jsx
+++ components/RightSide.jsx
@@ -1,6 +1,65 @@
 import React from "react";
 import "./style.css";
+import { CiSearch } from "react-icons/ci";
+import { CiBellOn } from "react-icons/ci";
+import { IoIosArrowDown } from "react-icons/io";
+import img from "../imgs/right imgs/unsplash_NohB3FJSY90.png";
+import Image from "next/image";
+
+const data1 = [
+  {
+    top: "Top 10",
+    middle: "Position in Dribble",
+    footer: "20% Increase from Last Week",
+  },
+
+  {
+    top: "26",
+    middle: "New employees onboarded",
+    footer: "15% Increase from Last Month",
+  },
+
+  {
+    top: "500",
+    middle: "New Clients Approached",
+    footer: "5% Increase from Last Week",
+  },
+];
 
 export const RightSide = () => {
-  return <div className="rightSide">RightSide</div>;
+  return (
+    <div className="rightSide">
+      <div className="top">
+        <CiSearch className="icon" />
+        <CiBellOn className="icon" />
+        <Image src={img} className="avatar" />
+        <IoIosArrowDown id="icon" />
+      </div>
+
+      <div className="middle">
+        {data1.map((item, index) => {
+          return (
+            <div className="box" key={index}>
+              <h3>{item.top}</h3>
+              <h4>{item.middle}</h4>
+              <h5>{item.footer}</h5>
+            </div>
+          );
+        })}
+      </div>
+      <div className="footer">
+        <div className="top">
+          <h3>Notifications</h3>
+          <p>View All</p>
+        </div>
+        <div className="box">
+          <Image />
+          <span>
+            <h4>Ellie joined team developers</h4>
+            <h5>04 April, 2021 | 04:00 PM</h5>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
